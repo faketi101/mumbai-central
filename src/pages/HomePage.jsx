bb24c76ddcabc5fd2df53b8d0588e24743f0e58c
Tarikul Islam <tarikul@tarikul.dev>
2025-10-07 19:29:22 +0600

    r3-fix-2

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -77,7 +77,7 @@ const HomePage = () => {
 
       {/* CTA section */}
       <div
-        id="reward"
+        id="rewards"
         className="cta w-screen flex flex-col items-center justify-center gap-4 bg-[var(--color-primary)] py-12 px-4 mt-4"
       >
         <h1 className="text-3xl md:text-4xl text-white uppercase font-bold text-center">
